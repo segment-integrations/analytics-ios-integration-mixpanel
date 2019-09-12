9a1fb198a8f1a98de94b45adb612d6c071d86c39
Evan Coleman <e@edc.me>
2019-09-12 16:30:45 -0400

    update header

--- Segment-Mixpanel/Segment_Mixpanel.h
+++ Segment-Mixpanel/Segment_Mixpanel.h
@@ -8,4 +8,5 @@ FOUNDATION_EXPORT const unsigned char Segment_MixpanelVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Segment_Mixpanel/PublicHeader.h>
 
-
+#import <Segment_Mixpanel/SEGMixpanelIntegration.h>
+#import <Segment_Mixpanel/SEGMixpanelIntegrationFactory.h>
