e44a8280d24785a8a4546659e77276e4b47197f6
Brie <brienne.mcnally@segment.com>
2020-01-07 16:47:44 -0800

    Comment back in all unit tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -34,109 +34,109 @@ describe(@"Mixpanel Integration", ^{
         [verify(mixpanel) track:@"App Opened" properties:@{}];
     });
 
-//    it(@"track with properties", ^{
-//        [integration track:[SEGPayloadBuilder track:@"Viewed Product" withProperties:@{
-//            @"sku" : @"123456"
-//        }]];
-//
-//        [verify(mixpanel) track:@"Viewed Product" properties:@{
-//            @"sku" : @"123456"
-//        }];
-//    });
-//
-//    it(@"track with people", ^{
-//        [integration setSettings:@{
-//            @"people" : @1
-//        }];
-//        [integration track:[SEGPayloadBuilder track:@"Purchased Item" withProperties:@{
-//            @"revenue" : @19.99
-//        }]];
-//
-//        [verify(mixpanel) track:@"Purchased Item" properties:@{
-//            @"revenue" : @19.99
-//        }];
-//        [verify(mixpanelPeople) trackCharge:@19.99];
-//    });
-//
-//    it(@"track with property increments", ^{
-//        [integration setSettings:@{
-//            @"people" : @1,
-//            @"propIncrements" : @[@"revenue"]
-//        }];
-//        [integration track:[SEGPayloadBuilder track:@"Purchased Item" withProperties:@{
-//            @"revenue" : @19.99
-//        }]];
-//        [verify(mixpanel) track:@"Purchased Item" properties:@{
-//            @"revenue" : @19.99
-//        }];
-//    });
-//
-//    it(@"screen", ^{
-//        [integration screen:[SEGPayloadBuilder screen:@"Home"]];
-//
-//        [verify(mixpanel) track:@"Viewed Home Screen" properties:@{}];
-//    });
-//
-//    it(@"screen with consolidatedPageCalls", ^{
-//        [integration setSettings:@{
-//            @"consolidatedPageCalls" : @1
-//        }];
-//        [integration screen:[SEGPayloadBuilder screen:@"Home"]];
-//
-//        [verify(mixpanel) track:@"Loaded a Screen" properties:@{@"name": @"Home"}];
-//    });
-//
-//    it(@"identify", ^{
-//        [integration identify:[SEGPayloadBuilder identify:@"prateek"]];
-//
-//        [verify(mixpanel) identify:@"prateek"];
-//    });
-//
-//    it(@"identify with traits", ^{
-//        [integration identify:[SEGPayloadBuilder identify:@"prateek" withTraits:@{
-//            @"firstName" : @"Prateek",
-//            @"lastName" : @"Srivastava",
-//            @"createdAt" : @"",
-//            @"lastSeen" : @"",
-//            @"email" : @"prateek@segment.com",
-//            @"name" : @"prateek srivastava",
-//            @"username" : @"f2prateek",
-//            @"phone" : @"",
-//            @"age" : @24
-//        }]];
-//
-//        [verify(mixpanel) identify:@"prateek"];
-//        [verify(mixpanel) registerSuperProperties:@{
-//            @"$first_name" : @"Prateek",
-//            @"$last_name" : @"Srivastava",
-//            @"$created" : @"",
-//            @"$last_seen" : @"",
-//            @"$email" : @"prateek@segment.com",
-//            @"$name" : @"prateek srivastava",
-//            @"$username" : @"f2prateek",
-//            @"$phone" : @"",
-//            @"age" : @24
-//        }];
-//    });
-//
-//    it(@"alias", ^{
-//        [given([mixpanel distinctId]) willReturn:@"123456"];
-//        [integration alias:[SEGPayloadBuilder alias:@"prateek"]];
-//
-//        [verify(mixpanel) createAlias:@"prateek" forDistinctID:@"123456"];
-//    });
-//
-//    it(@"flush", ^{
-//        [integration flush];
-//
-//        [verify(mixpanel) flush];
-//    });
-//
-//    it(@"reset", ^{
-//        [integration reset];
-//
-//        [verify(mixpanel) flush];
-//    });
+    it(@"track with properties", ^{
+        [integration track:[SEGPayloadBuilder track:@"Viewed Product" withProperties:@{
+            @"sku" : @"123456"
+        }]];
+
+        [verify(mixpanel) track:@"Viewed Product" properties:@{
+            @"sku" : @"123456"
+        }];
+    });
+
+    it(@"track with people", ^{
+        [integration setSettings:@{
+            @"people" : @1
+        }];
+        [integration track:[SEGPayloadBuilder track:@"Purchased Item" withProperties:@{
+            @"revenue" : @19.99
+        }]];
+
+        [verify(mixpanel) track:@"Purchased Item" properties:@{
+            @"revenue" : @19.99
+        }];
+        [verify(mixpanelPeople) trackCharge:@19.99];
+    });
+
+    it(@"track with property increments", ^{
+        [integration setSettings:@{
+            @"people" : @1,
+            @"propIncrements" : @[@"revenue"]
+        }];
+        [integration track:[SEGPayloadBuilder track:@"Purchased Item" withProperties:@{
+            @"revenue" : @19.99
+        }]];
+        [verify(mixpanel) track:@"Purchased Item" properties:@{
+            @"revenue" : @19.99
+        }];
+    });
+
+    it(@"screen", ^{
+        [integration screen:[SEGPayloadBuilder screen:@"Home"]];
+
+        [verify(mixpanel) track:@"Viewed Home Screen" properties:@{}];
+    });
+
+    it(@"screen with consolidatedPageCalls", ^{
+        [integration setSettings:@{
+            @"consolidatedPageCalls" : @1
+        }];
+        [integration screen:[SEGPayloadBuilder screen:@"Home"]];
+
+        [verify(mixpanel) track:@"Loaded a Screen" properties:@{@"name": @"Home"}];
+    });
+
+    it(@"identify", ^{
+        [integration identify:[SEGPayloadBuilder identify:@"prateek"]];
+
+        [verify(mixpanel) identify:@"prateek"];
+    });
+
+    it(@"identify with traits", ^{
+        [integration identify:[SEGPayloadBuilder identify:@"prateek" withTraits:@{
+            @"firstName" : @"Prateek",
+            @"lastName" : @"Srivastava",
+            @"createdAt" : @"",
+            @"lastSeen" : @"",
+            @"email" : @"prateek@segment.com",
+            @"name" : @"prateek srivastava",
+            @"username" : @"f2prateek",
+            @"phone" : @"",
+            @"age" : @24
+        }]];
+
+        [verify(mixpanel) identify:@"prateek"];
+        [verify(mixpanel) registerSuperProperties:@{
+            @"$first_name" : @"Prateek",
+            @"$last_name" : @"Srivastava",
+            @"$created" : @"",
+            @"$last_seen" : @"",
+            @"$email" : @"prateek@segment.com",
+            @"$name" : @"prateek srivastava",
+            @"$username" : @"f2prateek",
+            @"$phone" : @"",
+            @"age" : @24
+        }];
+    });
+
+    it(@"alias", ^{
+        [given([mixpanel distinctId]) willReturn:@"123456"];
+        [integration alias:[SEGPayloadBuilder alias:@"prateek"]];
+
+        [verify(mixpanel) createAlias:@"prateek" forDistinctID:@"123456"];
+    });
+
+    it(@"flush", ^{
+        [integration flush];
+
+        [verify(mixpanel) flush];
+    });
+
+    it(@"reset", ^{
+        [integration reset];
+
+        [verify(mixpanel) flush];
+    });
 });
 
 SpecEnd
